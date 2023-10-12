8f46c19490794a1f91cff891ee19cff9cca75914
ShernanJ <shernanjavier@gmail.com>
2023-10-12 00:13:18 -0400

    Added MongoDB integration

--- intern_net/pipelines.py
+++ intern_net/pipelines.py
@@ -1,3 +1,9 @@
+from dotenv import load_dotenv
+import pymongo
+import os
+
+load_dotenv()
+
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
@@ -10,4 +16,29 @@ from itemadapter import ItemAdapter
 
 class InternNetPipeline:
     def process_item(self, item, spider):
+        return item
+
+class MongoDBPipeline:
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+    
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=os.getenv("MONGO_URI"),
+            mongo_db=os.getenv("MONGO_DB")
+        )
+    
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+        
+    def close_spider(self, spider):
+        self.client.close()
+        
+    def process_item(self, item, spider):
+        collection = self.db['linkedin_jobs']
+        data = dict(item)
+        collection.insert_one(data)
         return item
